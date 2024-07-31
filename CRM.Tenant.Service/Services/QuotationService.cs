38c1b7a32f53ad57ea3f5eccc99eb817b7a55688
Harsh Dave <harshdave71720@gmail.com>
2024-07-31 17:52:21 +0530

    Added new service projet

---
+++ CRM.Tenant.Service/Services/QuotationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Tenant.Service.Services
+{
+    public class QuotationService
+    {
+        //public int CreationQuotation(Quotation quotation)
+        //{ 
+        
+        //}
+
+        //public Quotation GetQuoation(int id)
+        //{ 
+        //    DSASignatureDeformatter
+        //    return quotation;
+        //}
+
+        //public 
+    }
+}
